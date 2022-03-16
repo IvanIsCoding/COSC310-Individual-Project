1c182c3332e24336110f94a34b70f43870f8a23a
KafkaNoNeko <paula.wong-chung@alumni.ubc.ca>
2022-03-15 17:45:45 -0700

    add more misspelling tests. Will probably need an intent handler for each intent?

--- tests/test_misspellings.py
+++ tests/test_misspellings.py
@@ -26,6 +26,48 @@ class TestMisspellings(unittest.TestCase):
             self.assertTrue(
                 "CEO" in result["fulfillmentMessages"][0]["text"]["text"][0]
             )
+
+    def test_tesla(self):
+        """Test that a correct response is returned when 'tesla' is misspelt"""
+        app = Flask(__name__)
+
+        with app.app_context():
+            tesla_request = get_test_request("tesla_misspelt")
+            response = cloud_function(tesla_request)
+            result = json.loads(response.get_data(as_text=True))
             
+            self.assertTrue(
+                "Tesla" in result["fulfillmentMessages"][0]["text"]["text"][0]
+            )
+
+    def test_neuralink(self):
+        """Test that a correct response is returned when 'neuralink' is misspelt"""
+        app = Flask(__name__)
+
+        with app.app_context():
+            neu_request = get_test_request("neuralink_misspelt")
+            response = cloud_function(neu_request)
+            result = json.loads(response.get_data(as_text=True))
+            
+            self.assertTrue(
+                "Neuralink" in result["fulfillmentMessages"][0]["text"]["text"][0]
+            )       
+    
+    def test_university(self):
+        """Test that a correct response is returned when 'university' is misspelt"""
+        app = Flask(__name__)
+
+        with app.app_context():
+            uni_request = get_test_request("uni_misspelt")
+            response = cloud_function(uni_request)
+            result = json.loads(response.get_data(as_text=True))
+
+            print(result["fulfillmentMessages"][0]["text"]["text"][0])
+            
+            self.assertTrue(
+                "University of Pennsylvania" in result["fulfillmentMessages"][0]["text"]["text"][0]
+            )       
+
+    
 if __name__ == "__main__":
     unittest.main()
