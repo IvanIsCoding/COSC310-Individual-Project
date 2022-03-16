162076aa394c3e4a34826b971ac94a2dd568a1cd
KafkaNoNeko <paula.wong-chung@alumni.ubc.ca>
2022-03-15 17:15:08 -0700

    add a unit test for misspellings

---
+++ tests/test_misspellings.py
@@ -0,0 +1,31 @@
+import unittest
+import sys
+import os
+import json
+from flask import Flask
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "elonmusk"))
+
+from tests.mock_dialogflow_utils import *
+
+from elonmusk.main import cloud_function
+
+class TestMisspellings(unittest.TestCase):
+
+    def test_name(self):
+        """Test that a correct response is returned when 'name' is misspelt"""
+        app = Flask(__name__)
+
+        with app.app_context():
+            name_request = get_test_request("name_misspelt")
+            response = cloud_function(name_request)
+            result = json.loads(response.get_data(as_text=True))
+            
+            # Cannot use "Elon Musk" here since the default fallback message is 
+            # "My engineers are working on this right now - thanks for talking to Elon Musk Bot"
+            self.assertTrue(
+                "CEO" in result["fulfillmentMessages"][0]["text"]["text"][0]
+            )
+            
+if __name__ == "__main__":
+    unittest.main()
